b957d7086838ba7be78d1d799528df2aa676baf8
Brendan Boyle <brendancboyle@me.com>
2016-01-07 23:49:24 -0500

    Added redirect

---
+++ abtest.js
@@ -0,0 +1,8 @@
+var sites = [
+    "https://docs.google.com/a/ncsu.edu/forms/d/1XfwAVn9soDUDEEbDWYq2qE2rys8XSQFdEoQaR_IMM6k/viewform",
+    "https://docs.google.com/a/ncsu.edu/forms/d/1uAi06wFvJ1eXRFkhNxfAIIiHCPXvrLVpFvmskBfGt04/viewform"
+];
+
+var index = Math.floor((Math.random() * sites.length));
+
+window.location = sites[index];
